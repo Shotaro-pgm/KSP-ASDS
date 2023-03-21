f82166a9b75a6f6dd00725a477f2c10d28f76e51
Shotaro Ito <shotaro.ito.appleapple@gmail.com>
2023-03-21 16:44:46 +0900

    宇宙運輸部門のページを作成中です。

--- space-transport.php
+++ space-transport.php
@@ -2,4 +2,8 @@
 <div class="wrapper">
     <div><img src="img/2023-03-19 (1).png" alt="" id="section-image"></div>
 </div>
+<div class="wrapper">
+    <h1>運べ。素早く正確に。</h1>
+    <p>宇宙に行く手段といえばなんでしょう。そう、もちろんロケットですよね。<br></p>
+</div>
 <?php require 'footer.php'; ?>
